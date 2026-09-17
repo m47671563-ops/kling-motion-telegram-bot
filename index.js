e3079aec0b634b7cfae5575de9c4945884ea01af
m47671563-ops <m47671563@gmail.com>
2026-09-17 21:10:10 +0700

    Remove comment before tg function declaration

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// clean deploy    async function tg(env, method, body) {
+async function tg(env, method, body) {
   const url =
     `https://api.telegram.org/bot${env.TELEGRAM_BOT_TOKEN}/${method}`;
 
